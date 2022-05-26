28cf55be24c6ed051fcb2fd1bf1eef3f0dea45df
thenotandy <andyp@kdatasystems.net>
2022-05-26 10:50:04 -0400

    Initial version of PublicDNS monitor
    This monitor detects if client DNS servers are statically assigned to a public IP.

---
+++ PublicDNS.ps1
@@ -0,0 +1,11 @@
+﻿$interfaceID = (Get-NetIPConfiguration).NetProfile.InstanceID
+$interfaceKeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\$interfaceID"
+$pattern = "(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)"
+$string = Get-ItemProperty -Path $interfaceKeyPath -Name 'NameServer' | select  -ExpandProperty NameServer 
+$cnt = 1
+foreach ($detailexplain in $string.Split(","))
+{
+    $Internal = -join($Internal,$detailexplain | select-string  -Pattern $pattern -NotMatch)
+    $cnt++
+}
+$Internal
