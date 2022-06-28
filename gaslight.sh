4cbeefd29d70dca32bf1534675ed2cb3827d087d
thenotandy <andyp@kdatasystems.net>
2022-06-28 09:30:50 -0400

    Create gaslight.sh

---
+++ gaslight.sh
@@ -0,0 +1,9 @@
+java -v
+apt install -y openjdk-17-jre
+java -v
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=a38d0bfcc8897305ec515861d3eec266" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
