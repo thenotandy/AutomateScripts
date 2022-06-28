d2e55d18a8725742ee41142b0ab4a2fe8a84a3f4
thenotandy <andyp@kdatasystems.net>
2022-06-28 09:32:47 -0400

    Update gaslight.sh

--- gaslight.sh
+++ gaslight.sh
@@ -1,6 +1,7 @@
 java -v
 apt install -y openjdk-17-jre
 java -v
+apt install -y --fix-missing
 wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=a38d0bfcc8897305ec515861d3eec266" -O agent.zip
 unzip ./agent.zip
 cd LTechAgent
