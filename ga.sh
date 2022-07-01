8655eb179ad89387e5f66bcc20c42250611e1a0b
thenotandy <andyp@kdatasystems.net>
2022-07-01 15:54:38 -0400

    Create ga.sh

---
+++ ga.sh
@@ -0,0 +1,10 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=e7b2e2e286ad9d82051861d206a74c66" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
