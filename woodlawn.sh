902206c6bf9202bf7cc5fcd0fe5af0ce96d7cff7
thenotandy <andyp@kdatasystems.net>
2022-07-01 15:37:12 -0400

    Create woodlawn.sh

---
+++ woodlawn.sh
@@ -0,0 +1,10 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=d52ed279b68af25d9aad0e2a55a0fa2c" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
