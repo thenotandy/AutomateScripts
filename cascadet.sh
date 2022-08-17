c66db73ccfd1baabc46c3e9855ac80537736c48d
thenotandy <andyp@kdatasystems.net>
2022-08-17 10:34:22 -0400

    Create cascadet.sh

---
+++ cascadet.sh
@@ -0,0 +1,11 @@
+apt update
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=a5aab5da3e610adadfec75b226e6ffc5" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
