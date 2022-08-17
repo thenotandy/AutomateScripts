dd93470e66954570cf6a1843fceb46417ca19cc1
thenotandy <andyp@kdatasystems.net>
2022-08-17 10:37:05 -0400

    Update cascadet.sh

--- cascadet.sh
+++ cascadet.sh
@@ -3,7 +3,7 @@ java -version
 apt install -y openjdk-17-jre
 java -version
 apt install -y --fix-missing
-wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=a5aab5da3e610adadfec75b226e6ffc5" -O agent.zip
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=b0cf971f24c85cd615367defff7199a1" -O agent.zip
 unzip ./agent.zip
 cd LTechAgent
 chmod +x install.sh
