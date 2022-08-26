b8f592eb68fd3cd697b7f879ed1f79e591dbf770
thenotandy <andyp@kdatasystems.net>
2022-08-26 09:44:50 -0400

    Create devons.sh

---
+++ devons.sh
@@ -0,0 +1,11 @@
+apt update
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=8eaff587965dd06bc2223df0a78ccd69" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
