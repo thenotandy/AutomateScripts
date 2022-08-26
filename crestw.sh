12507b30dfaefed8ed714d8049810c20651abf36
thenotandy <andyp@kdatasystems.net>
2022-08-26 09:48:51 -0400

    Create crestw.sh

---
+++ crestw.sh
@@ -0,0 +1,11 @@
+apt update
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=c097d9fa200f13099cdfccc0f19d8caf" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
