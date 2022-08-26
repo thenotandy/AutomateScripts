4bda8ccf3d43f33ff693715b50c5bd80eccf5ec7
thenotandy <andyp@kdatasystems.net>
2022-08-26 09:36:59 -0400

    Create graceh.sh

---
+++ graceh.sh
@@ -0,0 +1,11 @@
+apt update
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=8dfa61d23b7985ea14bccfee872c1814" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
