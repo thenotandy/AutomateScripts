1034405be76336b7ef555430c5ad52bfbf07dbb6
thenotandy <andyp@kdatasystems.net>
2022-08-26 10:40:25 -0400

    Update briarw.sh

--- briarw.sh
+++ briarw.sh
@@ -2,7 +2,7 @@ java -version
 apt install -y openjdk-17-jre
 java -version
 apt install -y --fix-missing
-wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=c097d9fa200f13099cdfccc0f19d8caf" -O agent.zip
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=7ce55aaa23c98f67df5756f55a694509" -O agent.zip
 unzip ./agent.zip
 cd LTechAgent
 chmod +x install.sh
