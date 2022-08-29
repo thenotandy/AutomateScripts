7564650ef22c3cd93f533db429ba2c6ad0914e87
thenotandy <andyp@kdatasystems.net>
2022-08-29 09:07:44 -0400

    Create purit.sh

---
+++ purit.sh
@@ -0,0 +1,10 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=9e3b094ab3a136ff59451cbf519a8afd" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
