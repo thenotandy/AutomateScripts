e1bd671f02dedcc7e6df26bb251a3d5d9d1ff70b
thenotandy <andyp@kdatasystems.net>
2022-10-06 16:41:16 -0400

    Create springvale.sh

---
+++ springvale.sh
@@ -0,0 +1,10 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=d5a53f5f37053c6bca87cf94e7013325" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
