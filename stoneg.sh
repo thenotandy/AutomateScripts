eacb02c071c85716d78fa6f7076721bd073a35e3
thenotandy <andyp@kdatasystems.net>
2022-10-14 11:15:42 -0400

    Create stoneg.sh

---
+++ stoneg.sh
@@ -0,0 +1,10 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=acc4cba5f4f5d7c46d33512de6d7177a" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
