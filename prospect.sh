dc4bfe8d5768ed0ca2360c4df0b6a36ce11afdd7
thenotandy <andyp@kdatasystems.net>
2023-06-02 10:29:27 -0400

    Create prospect.sh

---
+++ prospect.sh
@@ -0,0 +1,10 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=fa8f073127984e595ea226627d73b56e" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
