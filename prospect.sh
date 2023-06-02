43d78aaf2e206410163a090fec1cea522ce9091e
thenotandy <andyp@kdatasystems.net>
2023-06-02 10:30:38 -0400

    Update prospect.sh

--- prospect.sh
+++ prospect.sh
@@ -2,7 +2,7 @@ java -version
 apt install -y openjdk-17-jre
 java -version
 apt install -y --fix-missing
-wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=fa8f073127984e595ea226627d73b56e" -O agent.zip
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=f5f5895a07e58e8252ade7c6a4897295" -O agent.zip
 unzip ./agent.zip
 cd LTechAgent
 chmod +x install.sh
