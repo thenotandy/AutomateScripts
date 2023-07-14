2a3d4246bed92b4621c9da5bb305b5cb7049b30d
thenotandy <andyp@kdatasystems.net>
2023-07-14 14:33:13 -0400

    Update PublicDNS.ps1
    Prevent flagging local DNS filtering systems.

--- PublicDNS.ps1
+++ PublicDNS.ps1
@@ -1,6 +1,6 @@
 ﻿$interfaceID = (Get-NetIPConfiguration).NetProfile.InstanceID
 $interfaceKeyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\$interfaceID"
-$pattern = "(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)"
+$pattern = "(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)|(^127\.0\.0\.)"
 $string = Get-ItemProperty -Path $interfaceKeyPath -Name 'NameServer' | select  -ExpandProperty NameServer 
 $cnt = 1
 foreach ($detailexplain in $string.Split(","))
