7a130ec3c2f7967ee94795b89b99072416ee2fca
thenotandy <andyp@kdatasystems.net>
2023-07-21 14:12:00 -0400

    Create chat.sh

---
+++ chat.sh
@@ -0,0 +1,10 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=8432dd71625c99be1df72bf0092cb9c4" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
