c699c5931247eb57b727d2412d89ce659d09a771
thenotandy <andyp@kdatasystems.net>
2023-09-18 13:29:44 -0400

    Create hhal.sh

---
+++ hhal.sh
@@ -0,0 +1,10 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=8afbd05898880cad81bf6cf1ea059a95" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
