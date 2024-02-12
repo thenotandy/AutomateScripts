43d1d2526ac1d7f8a394808a6f5aa508df0a39ba
thenotandy <andyp@kdatasystems.net>
2024-02-12 16:21:38 -0500

    Create boling.sh

---
+++ boling.sh
@@ -0,0 +1,10 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=f28d701e5767d1bb3c82f99a35cfe46c" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
