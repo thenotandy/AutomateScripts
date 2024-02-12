560a2a0702db70a5ebcfc1865758aa3ba52e721e
thenotandy <andyp@kdatasystems.net>
2024-02-12 16:52:26 -0500

    Update boling.sh

--- boling.sh
+++ boling.sh
@@ -8,3 +8,8 @@ cd LTechAgent
 chmod +x install.sh
 ./install.sh
 sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
+
+DEBIAN_FRONTEND=noninteractive apt-get install lightdm -y
+echo "/usr/sbin/lightdm" > /etc/X11/default-display-manager
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true dpkg-reconfigure lightdm
+echo set shared/default-x-display-manager lightdm | debconf-communicate
