cf5bfdee79a50b2bc0af94db08336e81b55caa41
thenotandy <andyp@kdatasystems.net>
2024-07-12 15:55:13 -0400

    Create birm.sh

---
+++ birm.sh
@@ -0,0 +1,15 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=023b989f02401a3c705de2bf4943b0d2" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
+
+DEBIAN_FRONTEND=noninteractive apt-get install lightdm -y
+echo "/usr/sbin/lightdm" > /etc/X11/default-display-manager
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true dpkg-reconfigure lightdm
+echo set shared/default-x-display-manager lightdm | debconf-communicate
