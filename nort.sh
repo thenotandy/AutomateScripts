762eaefbfef1b83d7d64d33fef525132720a365e
thenotandy <andyp@kdatasystems.net>
2024-07-24 11:58:06 -0400

    Create nort.sh

---
+++ nort.sh
@@ -0,0 +1,15 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=c2e2f009fa7aa6dab6f9570ca3d88f47" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
+
+DEBIAN_FRONTEND=noninteractive apt-get install lightdm -y
+echo "/usr/sbin/lightdm" > /etc/X11/default-display-manager
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true dpkg-reconfigure lightdm
+echo set shared/default-x-display-manager lightdm | debconf-communicate
