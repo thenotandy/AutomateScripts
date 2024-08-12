746edf175ccf6176d0ef582868a94c75fbdad9bc
thenotandy <andyp@kdatasystems.net>
2024-08-12 11:42:17 -0400

    Create nov.sh

---
+++ nov.sh
@@ -0,0 +1,15 @@
+java -version
+apt install -y openjdk-17-jre
+java -version
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=3c6d5c1212d6de4d9c30bcefb45d5945" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
+
+DEBIAN_FRONTEND=noninteractive apt-get install lightdm -y
+echo "/usr/sbin/lightdm" > /etc/X11/default-display-manager
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true dpkg-reconfigure lightdm
+echo set shared/default-x-display-manager lightdm | debconf-communicate
