bfed066069a8a6104dd487d31b42a47624a46525
thenotandy <andyp@kdatasystems.net>
2025-06-26 17:18:59 -0400

    Update oxford.sh

--- oxford.sh
+++ oxford.sh
@@ -1,1 +1,6 @@
-
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=1f72edaaf9e5669e9c42d2cb0e5b3d78" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
