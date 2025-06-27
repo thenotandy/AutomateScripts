78322a9fe62edae07974a10d36e1675e89879da0
thenotandy <andyp@kdatasystems.net>
2025-06-27 09:10:52 -0400

    Update lwoods.sh

--- lwoods.sh
+++ lwoods.sh
@@ -1,5 +1,5 @@
 apt install -y --fix-missing
-wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=3da69af6ec95d379a10dc386361fd14a -O agent.zip
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=3da69af6ec95d379a10dc386361fd14a" -O agent.zip
 unzip ./agent.zip
 cd LTechAgent
 chmod +x install.sh
