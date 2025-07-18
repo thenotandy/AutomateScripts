aee08f660e64481cb1e041f130919b1e1482529e
thenotandy <andyp@kdatasystems.net>
2025-07-18 16:19:00 -0400

    Create forestg.sh

---
+++ forestg.sh
@@ -0,0 +1,6 @@
+apt install -y --fix-missing
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=a51ae7e15de5590f6ecbaa197e134939" -O agent.zip
+unzip ./agent.zip
+cd LTechAgent
+chmod +x install.sh
+./install.sh
