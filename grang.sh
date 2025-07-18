6ad02e2de915c727d5222f2b4b2ea1ba8afec63a
thenotandy <andyp@kdatasystems.net>
2025-07-18 15:44:02 -0400

    Update grang.sh

--- grang.sh
+++ grang.sh
@@ -1,8 +1,5 @@
-java -version
-apt install -y openjdk-17-jre
-java -version
 apt install -y --fix-missing
-wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=b121db919d1910a93cdf26212b2acc90" -O agent.zip
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=3927edc1c1098c95ff87197de51f530d" -O agent.zip
 unzip ./agent.zip
 cd LTechAgent
 chmod +x install.sh
