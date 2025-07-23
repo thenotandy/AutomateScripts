38d20b261df442711852df35fe1f806618f849cf
thenotandy <andyp@kdatasystems.net>
2025-07-23 10:46:12 -0400

    Update ludington.sh

--- ludington.sh
+++ ludington.sh
@@ -1,10 +1,6 @@
-java -version
-apt install -y openjdk-17-jre
-java -version
 apt install -y --fix-missing
-wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=a3e45b103452c8d2ca44a5268982418f" -O agent.zip
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=aa118780c1902d6839c6a9af1842a85a" -O agent.zip
 unzip ./agent.zip
 cd LTechAgent
 chmod +x install.sh
 ./install.sh
-sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
