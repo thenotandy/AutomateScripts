6f51b8afbe6145e98aa0858499ba80b254153dea
thenotandy <andyp@kdatasystems.net>
2025-07-23 10:45:29 -0400

    Update gaslight.sh

--- gaslight.sh
+++ gaslight.sh
@@ -1,10 +1,6 @@
-java -version
-apt install -y openjdk-17-jre
-java -version
 apt install -y --fix-missing
-wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=fa8f073127984e595ea226627d73b56e" -O agent.zip
+wget "https://kgroupcompanies.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=9589d18f29c85468e2ee513e37a9fb65" -O agent.zip
 unzip ./agent.zip
 cd LTechAgent
 chmod +x install.sh
 ./install.sh
-sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm3/custom.conf
